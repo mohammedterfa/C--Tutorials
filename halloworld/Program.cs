960d34253c75032d55d657d8a60a23a468912bc3
mohammedterfa <mohammedterfa@gmail.com>
2022-06-16 14:06:29 +0200

    if else program tutorial

--- halloworld/Program.cs
+++ halloworld/Program.cs
@@ -1,4 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-Console.WriteLine("first code");
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            bool isTall = false;
+            
+            if (isMale && isTall)
+            {
+                Console.WriteLine("you are tall male");
+            }
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("you are tall not male");
+            }
+
+            else if (isMale && !isTall) {
+                Console.WriteLine("you are short male");
+            }
+            else { 
+                Console.WriteLine("you are not tall not male then you are a woman");
+            }
+        }
+    }
+}
