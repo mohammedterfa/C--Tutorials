fc31a14875acbdd760d487ac50faf456893b98c1
mohammedterfa <mohammedterfa@gmail.com>
2022-06-17 12:01:54 +0200

    Write a C# program to check two given integers and return true if one is negative and one is positive

--- halloworld/Program.cs
+++ halloworld/Program.cs
@@ -12,18 +12,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string str;
+            Console.Write("input first number : ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Input a string");
-            str = Console.ReadLine();
+            Console.Write("input first number : ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-            if (str.Length > 1)
+            Console.WriteLine("check if one is negative and one is positive : " +CheckNumber(firstNumber,secondNumber));
+
+
+
+
+        }
+
+        public static bool CheckNumber(int num1, int num2) {
+            if (num1 > 0 && num2 > 0)
             {
-                var s = str.Substring(0,1);
-                Console.WriteLine(s+str+s);
+                return false;
             }
-    
-
-        }     
+            else if (num1 < 0 && num2 < 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }        
     }
 }
