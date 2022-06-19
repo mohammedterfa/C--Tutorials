1f7d2630027abbc1d4ebb4375da91232f524597d
mohammedterfa <mohammedterfa@gmail.com>
2022-06-19 14:50:49 +0200

    Compute the sum of the digits of an integer

--- halloworld/Program.cs
+++ halloworld/Program.cs
@@ -11,37 +11,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            long sum = 0;
-            int ctr = 0;
+            Console.WriteLine("Input an integer : ");
+            int num = Convert.ToInt32(Console.ReadLine());
 
-            int n = 2;
+            int sum = 0;
 
-            while (ctr < 500)
-            {
-                if (isPrime(n))
-                {
-
-                    sum += n;
-                    ctr++;
-                }
-                n++;
+            while (num != 0) {
+                sum += num % 10;
+                num /= 10;
             }
 
             Console.WriteLine(sum);
-        }
-
-        public static bool isPrime(int n) {
-            int x = (int)Math.Floor(Math.Sqrt(n));
-
-            if (n == 1) return false;
-            if(n == 2) return true;
-
-            for (int i = 2; i <= x; i++){
-                if (n % i == 0) return false;
-            }
-
-            return true;
-        
         }
 
     }        
