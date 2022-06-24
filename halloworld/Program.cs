452ea74d0d9ab8bfad77ab2b5f761dea5158cc96
mohammedterfa <mohammedterfa@gmail.com>
2022-06-24 21:33:12 +0200

    Shell sort

--- halloworld/Program.cs
+++ halloworld/Program.cs
@@ -7,27 +7,63 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    struct w3struct {
-        public int x;
-        public int y;
-    }
+  
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("declariate a simple structure ");
-            Console.WriteLine("-------------------------------");
 
-            w3struct w3 = new w3struct();
+            int[] arr = new int[] {5, -4, 11, 0, 18, 22, 67, 51, 6 };
+            int n;
 
-            w3.x = 15;
-            w3.y = 25;
+            n = arr.Length;
 
-            int sum = w3.x + w3.y;
+            Console.WriteLine("Original Array elements");
+            showArrayElements(arr);
 
-            Console.WriteLine("the sum of x and y is " + sum);
+            shellSort(arr, n);
+
+            Console.WriteLine("\nSorted Array elements");
+            showArrayElements(arr);
+            
         }
 
+        static void shellSort(int[] arr, int array_size)
+        {
+            int i, j, inc, temp;
+            inc = 3;
+            while (inc > 0)
+            {
+                for (i = 0; i < array_size; i++)
+                {
+                    j = i;
+                    temp = arr[i];
+                    while ((j >= inc) && (arr[j - inc] > temp))
+                    {
+                        arr[j] = arr[j - inc];
+                        j = j - inc;
+                    }
+                    arr[j] = temp;
+                }
+                if (inc / 2 != 0)
+                    inc = inc / 2;
+                else if (inc == 1)
+                    inc = 0;
+                else
+                    inc = 1;
+            }
+        }
+
+
+        static void showArrayElements(int[] arr) {
+            foreach (var element in arr)
+            {
+                Console.Write(element + " ");
+            }
+
+
+        }
 
     }
 }
